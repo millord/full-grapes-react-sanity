8eeb7624d6399e5cca601d6eb6dc4707cb3072e3
millord <juancarlosmillord@gmail.com>
2021-03-02 15:14:00 -0400

    added all the three repos first commit

---
+++ sanity/schemas/templateData.js
@@ -0,0 +1,25 @@
+// in templateData.js
+export default {
+  name: "templateData",
+  title: "templateData",
+  type: "document",
+  fields: [
+    // ... other fields ...
+    {
+      name: "title",
+      title: "site title",
+      type: "string",
+    },
+    {
+      name: "html",
+      title: "html content",
+      type: "string",
+    },
+    {
+      name: "css",
+      title: "css content",
+      type: "string",
+    },
+   
+  ],
+};
